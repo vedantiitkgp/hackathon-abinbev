d67aa0137e8d23e62ba3ea26cf01fe338d068627
mukurgupta <38879291+mukurgupta@users.noreply.github.com>
2020-06-06 19:06:06 +0530

    Update the response

--- maverick/api/timelog.py
+++ maverick/api/timelog.py
@@ -13,39 +13,40 @@ class timelogfun:
 
         url=''.join([t1,str(EmployeeID),t2,fname,t3,lname,t4,email,t5])
         response=requests.post(url)
-        #return url
-        return response.text
+        res={'msg':response.text,'data':NULL}
+        return res
 
     def all_employees():
         params={'authtoken': '31d865f824a8c223a558ef393fe59a21'}
         url='https://people.zoho.in/people/api/forms/P_EmployeeView/records'
         response=requests.post(url, params=params)
-
-        return pd.read_json(response.text)[['First Name','Last Name','Email ID','recordId','EmployeeID','Employee Role']]
+        
+        res={'msg':NULL,'data':pd.read_json(response.text)[['First Name','Last Name','Email ID','recordId','EmployeeID','Employee Role']]}
+        return res
 
     def search_employee(email):
         t1='https://people.zoho.in/people/api/forms/P_EmployeeView/records?authtoken=31d865f824a8c223a558ef393fe59a21&searchColumn=EMPLOYEEMAILALIAS&searchValue='
         url=''.join([t1,str(email)])
-
         response=requests.post(url)
-
-        return pd.read_json(response.text)[['First Name','Last Name','Email ID','EmployeeID','Employee Role']]
-
+        res={'msg':NULL,'data':pd.read_json(response.text)[['First Name','Last Name','Email ID','EmployeeID','Employee Role']]}
+        return res
 
     def all_jobs(value):
         t='https://people.zoho.in/people/api/timetracker/getjobs?authtoken=31d865f824a8c223a558ef393fe59a21&assignedTo='
         url=''.join([t,str(value)])
         response=requests.post(url)
-
-        return pd.DataFrame(pd.read_json(response.text).iloc[1,0])[['jobName','jobStatus','fromDate','jobId']]
+        
+        res={'msg':NULL,'data':pd.DataFrame(pd.read_json(response.text).iloc[1,0])[['jobName','jobStatus','fromDate','jobId']]}
+        return res
 
     def change_job_status(jobid,status):
         t='https://people.zoho.in/people/api/timetracker/modifyjobstatus?authtoken=31d865f824a8c223a558ef393fe59a21&jobId='
         t1='&jobStatus='
         url=''.join([t,jobid,t1,status])
         response=requests.post(url)
-
-        return response.text
+        
+        res={'msg':response.text,'data':NULL}
+        return res
 
     def add_job(user,jobName,workdate,hrs):
         t='http://people.zoho.in/people/api/timetracker/addtimelog?authtoken=31d865f824a8c223a558ef393fe59a21&user='
@@ -56,8 +57,9 @@ class timelogfun:
 
         url=''.join([t,str(user),t1,jobName,t2,workdate,t3,t4,str(hrs)])
         response=requests.post(url)
-
-        return response.text
+        
+        res={'msg':response.text,'data':NULL}
+        return res
 
     def get_timelog(user,fromDate,toDate):
         t1='http://people.zoho.in/people/api/timetracker/gettimelogs?authtoken=31d865f824a8c223a558ef393fe59a21&user='
@@ -67,5 +69,6 @@ class timelogfun:
 
         url=''.join([t1,user,t2,fromDate,t3,toDate,t4])
         response=requests.post(url)
-
-        return pd.DataFrame(pd.read_json(response.text).iloc[1,0])[['employeeMailId','employeeFirstName','workDate','timelogId','jobName','jobIsActive','jobIsCompleted','hours']]
+        
+        res={'msg':NULL,'data':pd.DataFrame(pd.read_json(response.text).iloc[1,0])[['employeeMailId','employeeFirstName','workDate','timelogId','jobName','jobIsActive','jobIsCompleted','hours']]}
+        return res
