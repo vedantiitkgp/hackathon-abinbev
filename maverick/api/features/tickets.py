80a837ffdc356bbb6131979f3729f9183fd75ef6
Kashish Goyal <goyalkashish@iitkgp.ac.in>
2020-06-08 16:51:08 +0530

    minor mistakes

--- maverick/api/features/tickets.py
+++ maverick/api/features/tickets.py
@@ -1,26 +1,29 @@
 import requests
+import pandas as pd
+import json
 
 class ticket_func:
 	def __init__(self):
 		#SCOPE = Desk.settings.READ,Desk.basic.READ,Desk.tickets.ALL,Desk.contacts.READ
 		self.baseurl = "https://desk.zoho.in/api/v1"
 		self.orgid = "60004833729"
-		self.token = "1000.b63eaf9bf2e61351a4a704674076e069.2936d9ad0762837da03362d6fffb97ff"
+		self.token = "1000.2de677261948793e7993caa3e30cde26.e47f2de9a2ecbc47142c7e1437bf934b"
 		
 		self.headers = {'orgId': self.orgid, 'Authorization': 'Zoho-oauthtoken '+self.token}
 		# self.customerId = '26154000000064365'
 		# self.deptId = '26154000000010772'
 
-	def create_ticket(self, customerId, deptIdtitle):
+	def create_ticket(self, customerId, deptId, title):
 		title = title.replace('"', '')
 		data = {'subject': title, 'departmentId': deptId, 'contactId': customerId}
 		response = requests.post(self.baseurl+'/tickets', json = data, headers = self.headers)
-		output = {'msg': response.text, 'data': None}
+		msg = 'Ticket created successfully' if response.status_code == 200 else 'Some error while processing.'
+		output = {'msg': msg, 'data': None}
 		return output
 	
 	def show_tickets(self):
 		response = requests.get(self.baseurl+'/tickets', headers = self.headers)
-		return {'msg': None, 'data': pd.DataFrame(json.loads(response.text)['data'])[['id', 'ticketNumber', 'email', 'phone', 'subject', 'status', 'createdTime']]}
+		return {'msg': None, 'data': pd.DataFrame(json.loads(response.text)['data'])[['id', 'ticketNumber', 'subject', 'status', 'createdTime']]}
 
 	def close_ticket(self, ticketId):
 		data = {'ids': [ticketId]}
--- maverick/api/mid.py
+++ maverick/api/mid.py
@@ -1,11 +1,12 @@
 from .features.timelog import timelogfun
 from .features.tickets import ticket_func
 from .features.covidcases import covidcasesfun
-from .features.expense_managment import expense_managment
+from .features.expense_managment import expense_management
 
 class mediatorCall:
     def __init__(self,msg):
         self.msg=msg
+        self.ticket_func = ticket_func();
 
     def run_query(self):
         msg=self.msg.split()
@@ -29,72 +30,61 @@ class mediatorCall:
     def command_add(self,msg):
         if(msg[1]=='employee' or msg[1]=='Employee'):
             output=timelogfun.add_employee(msg[2],msg[3],msg[4],msg[5])
-            return output
-
         elif(msg[1]=='job' or msg[1]=='Job'):
             output=timelogfun.add_job(msg[4],msg[2],msg[6],msg[8])
-            return output
         elif(msg[1].lower=='expense'):
             output=expense_management.create_expense(msg[3],msg[7],msg[5])
-            return output
+        return output
 
     def command_show(self,msg):
         if(msg[1].lower()=='employee'):
             output=timelogfun.all_employees()
-            return output
         elif(msg[1].lower()=='jobs'):
             output=timelogfun.all_jobs(msg[3])
-            return output
         elif(msg[1].lower()=='timelog'):
             output=timelogfun.get_timelog(msg[3],msg[5],msg[7])
-            return output
         elif(msg[1].lower()=='tickets'):
-            output=ticket_func.show_tickets()
+            output=self.ticket_func.show_tickets()
         elif(msg[1].lower()=='departments'):
-            output=ticket_func.show_departments()
+            output=self.ticket_func.show_departments()
         elif(msg[1].lower()=='customers'):
-            output=ticket_func.show_customers()
+            output=self.ticket_func.show_customers()
         elif(msg[1].lower=='covid'):
             output=covidcasesfun.cases(msg[4],msg[5])
-            return output
         elif(msg[1].lower=='expenses'):
             output=expense_management.list_expenses()
-            return output
         elif(msg[1].lower=='expense'):
             output=expense_management.list_expense_history(msg[2])
-            return output
         elif(msg[1].lower=='accounts'):
             output=expense_management.list_accounts()
-            return output
+        return output
 
     def command_search(self,msg):
-            if(msg[1]=='employee' or msg[1]=='Employee'):
-                output=timelogfun.search_employee(msg[2])
-                return output
-            elif(msg[1].lower=='expense'):
-                output=expense_management.get_expense(msg[2])
-                return output
+        if(msg[1]=='employee' or msg[1]=='Employee'):
+            output=timelogfun.search_employee(msg[2])
+        elif(msg[1].lower=='expense'):
+            output=expense_management.get_expense(msg[2])
+        return output
 
     def command_change(self,msg):
         if(msg[1]=='job' or msg[1]=='Job'):
             output=timelogfun.change_job_status(msg[4],msg[6])
-            return output
         elif(msg[1].lower=='expense'):
             output=expense_management.update_expense(msg[2],msg[4],msg[6],msg[8])
-            return output
+        return output
 
     def command_create(self, msg):
         if (msg[1].lower() == 'ticket'):
             #create ticket by :customerId for :departmentId - ":title"
-            output = ticket_func.create_ticket(msg[3], msg[5], ' '.join(msg[7:]))
-            return output
+            output = self.ticket_func.create_ticket(msg[3], msg[5], ' '.join(msg[7:]))
+        return output
 
     def command_close(self, msg):
         if (msg[1].lower() == 'ticket'):
-            #close ticket :ticketID
-            output = ticket_func.close_ticket(msg[2])
+            output = self.ticket_func.close_ticket(msg[2])
+        return output
             
     def command_delete(self,msg):
         if(msg[1].lower()=='expense'):
             output=expense_management.delete_expense(msg[2])
-            return output
+        return output
