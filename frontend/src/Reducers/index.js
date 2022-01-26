f8727bf4b38d75a4d4dc7222585d0bafe3eee1b2
Vedant saraswat <32813089+vedantiitkgp@users.noreply.github.com>
2022-01-26 09:18:41 +0530

    Revert "Temp (#1)" (#2)
    This reverts commit 740b65ef0d113aea6a193a9971fb17efdfded14a.

--- frontend/src/Reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import messages from "./messages";
-
-const reducers = combineReducers({
-    messages:messages,
-});
-
-export default reducers;
--- maverick/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import MessagesView, WebHookView
-
-urlpatterns = [
-    # path('getmessages/', GetMessages.as_view()),
-    path('sendMessage', MessagesView.as_view()),
-    path('webhook', WebHookView)
-]
